7a60fdde399e656353355e53b6ae6a8c009c0c4b
AnaRenchen <anamagbh@gmail.com>
2024-01-12 14:52:46 -0300

    crear archivo productos

---
+++ prueba/src/asyncMock.jsx
@@ -0,0 +1,13 @@
+import Orochimaru from "../src/assets/imagenes/orochimaru.jpg";
+
+const products = [
+  {
+    id: 1,
+    name: "Orochimaru 大蛇丸",
+    price: "$300",
+    category: "characters",
+    img: { Orochimaru },
+    stock: 5,
+    description: "Original A3 Size Painting",
+  },
+];
