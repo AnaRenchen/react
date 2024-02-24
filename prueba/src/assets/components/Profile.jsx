6031b3be841340f00f5b4d021440c62a0615b4b8
AnaRenchen <anamagbh@gmail.com>
2024-02-24 11:10:09 -0300

    correccion profile

--- prueba/src/assets/components/Profile.jsx
+++ prueba/src/assets/components/Profile.jsx
@@ -4,10 +4,10 @@ const Profile = () => {
       <p className="texto_profile">
         Santu Horisada studied from an early age 1 year of drawing, 4 years of
         manga and 1 year of sumi-e. He started tattooing in 2007. His work is
-        characterized by having a great content of symbology, the result of its
-        his personal interest in the history of Asia, which ranges from
-        mythology, Buddhism, Shinto, legends, novels, wars, Japanese folklore,
-        among others. The pursuit of his work is to maintain the richness of the
+        characterized by having a great content of symbology, the result of his
+        personal interest in the history of Asia, which ranges from mythology,
+        Buddhism, Shinto, legends, novels, wars, Japanese folklore, among
+        others. The pursuit of his work is to maintain the richness of the
         culture of the traditional Japanese tattoo without deviating too much,
         but also imparting his own vision of tattooing, according to his studies
         both from research in books, as well as from his trips to Japan.
